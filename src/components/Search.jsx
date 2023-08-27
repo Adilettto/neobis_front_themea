033880bf70c3d1bc79510d29e703b5772d9c8c88
Adilet <adilet.zamirbekov.kg@gmail.com>
2023-08-27 14:15:30 -0500

    added search functionality by name

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,17 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import "./Search.css";
+import ratataImg from "../assets/ratata.jpg";
 
 const Search = () => {
+  const [searchInput, setSearchInput] = useState("");
+  const [filteredMeals, setFilteredMeals] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const handleInputChange = (e) => {
+    setSearchInput(e.target.value);
+  };
+
+  const handleSearch = () => {
+    setLoading(true);
+    axios(`https://www.themealdb.com/api/json/v1/1/search.php?s=${searchInput}`)
+      .then(({ data }) => {
+        setFilteredMeals(data.meals || []);
+        setLoading(false);
+      })
+      .catch((err) => {
+        console.log(err);
+        setLoading(false);
+      });
+  };
+
   return (
-    <div className="search-bar">
-      <h3>Find your Meal</h3>
-      <div className="search-field">
-        <input
-          className="search-input"
-          type="text"
-          placeholder="Enter your dish"
-        />
-        <button className="search-btn">Search</button>
+    <div className="search-block">
+      <div className="search-bar">
+        <h3>Find your Meal</h3>
+        <div className="search-field">
+          <input
+            className="search-input"
+            type="text"
+            placeholder="Enter your dish"
+            onChange={handleInputChange}
+          />
+          <button onClick={handleSearch} className="search-btn">
+            Search
+          </button>
+        </div>
+      </div>
+      <div className="meal-list">
+        {loading && <p>Loading...</p>}
+        {!loading && 
+          filteredMeals.map((meal) => (
+            <div className="meal-item" key={meal.idMeal}>
+              <img src={meal.strMealThumb} alt="meal" />
+              <div className="meal-name-short">
+                <h3>{meal.strMeal}</h3>
+                <p>{meal.strCategory} | {meal.strArea}</p>
+              </div>
+            </div>
+          ))
+        }
       </div>
     </div>
   );
